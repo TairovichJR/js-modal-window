fce1f0e0f771a7d8579607ee4c4023547f48d4a5
TairovichJR <yokubov.omonjon@gmail.com>
2020-11-19 18:29:59 -0500

    code refactor and esc key listener

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ const closeModal = function () {
 };
 
 const openModal = function () {
-  console.log('Button clicked');
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -24,3 +23,10 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 btnCloseModal.addEventListener('click', closeModal);
 
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
